50c41e613db22f4fbb0108f4b97a6f39cc4eb1b8
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-10-27 15:58:25 +0530

    Completed input box

--- currencyConverter/src/components/Input.jsx
+++ currencyConverter/src/components/Input.jsx
@@ -1,3 +1,5 @@
+import { useId  } from "react";
+
 
 function InputBox({
     label,
@@ -10,16 +12,17 @@ function InputBox({
     className = "",
     currencyDisable
 }) {
-   
+   const amountInputId = useId();
+
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id = {amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -38,8 +41,8 @@ function InputBox({
                 >
                     
                         {currencyOption.map((curr) => (
-                            <option value="" key={}>
-
+                            <option value={curr} key={curr}>
+                            {curr}
                             </option>
                         ))}
                 
